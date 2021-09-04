11abb0fdff45819e8eb7a8ab9e341a83ccf0e736
{Sakshi Singh} <{babbysingh7319@gmail.com}>
2021-09-04 13:21:31 +0530

    Dashboard and plotly

---
+++ mydash.py
@@ -0,0 +1,17 @@
+import dash
+
+import dash_core_components as dcc
+import dash_html_components as html
+
+app= dash.Dash()
+
+app.layout = html.Div(children=[
+            html.H1('Hello Dash')
+            dcc.Graph(id='example',
+            figure={'data'
+
+
+])
+
+if __name__ == '__main__':
+    app.run_server()
